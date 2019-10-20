3445a78add5a610f28551ca39c2d491a987d10c6
CarlTondo9 <54605982+CarlTondo9@users.noreply.github.com>
2019-10-20 00:01:31 -0400

    Add files via upload

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
 @app.route('/')
